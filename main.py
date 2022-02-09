2a565d5c7bb16e96169c03cacacfe662cf943e1e
jmutt78 <jmcintosh@docusites.com>
2022-02-09 15:51:49 -0800

    loose specs

---
+++ main.py
@@ -0,0 +1,22 @@
+# check for a file 
+# if a file exists, load the data from the file. 
+# test the data for name, category, data. 
+# if it is missing one of them, throw an error with what's missing. 
+# if the file does not exist, or after the data is loaded, prompt the user. 
+
+# prompt the expense name 
+# prompt the expense category 
+# prompt the Expense date 
+# prompt the user if to add more? 
+# store the data into an object in an array
+
+# if the user needs to add more, repeat prompts.
+# if the user says no, then prompt the user for their projected income for the month. 
+# store the income as a variable,
+# add all the expenses, and store the total as a variable 
+# store the difference of the total and the expenses 
+# write them in a file and also print them in a formatted way to the console.
+
+# check for duplicate expenses 
+# give them a breakdown for their expenses in each category 
+# give them a breakdown of in a list of expenses 
