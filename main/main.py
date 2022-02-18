fa4f7ff3a0961ae8e7a4ebf72bdbb06facb7b8a7
jmutt78 <jmcintosh@docusites.com>
2022-02-18 06:54:34 -0800

    more fixes

--- main/main.py
+++ main/main.py
@@ -10,16 +10,19 @@ if __name__ == '__main__':
         option_3 = 'Enter 3 to analyze the expenses'
         option_4 = 'Enter 4 to reset the expenses'
         option_5 = 'Enter 5 to exit'
-        message = int(
-            input(f"{option_1}\n{option_2}\n{option_3}\n{option_4}\n{option_5}\n\nPlease select an option: "))
+        try:
+            message = int(
+                input(f"{option_1}\n{option_2}\n{option_3}\n{option_4}\n{option_5}\n\nPlease select an option: "))
 
-        if message == 1:
-            analyser_obj.gather_exp()
-        elif message == 2:
-            analyser_obj.csv_analyze()
-        elif message == 3:
-            analyser_obj.analyze_expenses()
-        elif message == 4:
-            analyser_obj.reset()
-        else:
-            break
+            if message == 1:
+                analyser_obj.gather_exp()
+            elif message == 2:
+                analyser_obj.csv_analyze()
+            elif message == 3:
+                analyser_obj.analyze_expenses()
+            elif message == 4:
+                analyser_obj.reset()
+            else:
+                break
+        except ValueError:
+            print('Please enter a valid option')
